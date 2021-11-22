3751726b28a523228cfeb246766bd22e0440e814
maheshjittaveni <maheshjittaveni46@gmail.com>
2021-11-22 11:15:51 +0530

    adding program.js

--- program.js
+++ program.js
@@ -2,3 +2,13 @@ var age=30;
 var num=40;
 print(age);
 console.log(num);
+
+
+function display(){
+    var day="monday";//local variable or local scope
+    console.log(day);
+}
+
+function printData(){
+    console.log(day);
+}
